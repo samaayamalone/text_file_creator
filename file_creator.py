65dfe139c111541f4cde93e811aa6c014114a874
Samaya <samayadenise@gmail.com>
2025-06-16 16:28:13 -0400

    changed number to fruit

--- file_creator.py
+++ file_creator.py
@@ -1,10 +1,9 @@
-# Prompt user for their name and favorite number
-name = input("What's your name? \n")
-number = input("What's your favorite number? \n")
 
-# Open 'example.txt' in write mode and write the user's input
+name = input("What's your name? \n")
+fruit = input("What's your favorite fruit? \n")
+
 with open('example.txt', 'w') as file:
     file.write(f"{name} created this file.\n")
-    file.write(f"{name}'s favorite number is {number}.")
+    file.write(f"{name}'s favorite fruit is {fruit}.")
 
   
